e48f4975401989db31678d87cf23186d0837cad2
FraserMuir <frasermuir14@gmail.com>
2023-02-08 21:40:11 +0000

    add cron and remove next

---
+++ runAutomation.spec.js
@@ -0,0 +1,14 @@
+import { test } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("https://members.liveargyll.co.uk/");
+  await page.click('css=[data-bs-target="#loginModal"]');
+  await page.fill("css=[id='member-email']", process.env.USERNAME);
+  await page.fill("css=[id='member-password']", process.env.PASSWORD);
+  await page.locator("css=button[type='submit']").filter({ hasText: "Log In" }).click();
+  await page.locator("css=a[href='/members-area/book/']").filter({ hasText: "Book a Class" }).click();
+  await page.click("#day-tab-7");
+  await page.locator("css=.tab-pane.fade.active.show").locator("text=Zumba").click();
+  await page.locator("css=button[type='button']").filter({ hasText: "Book Now" }).click();
+  await page.locator("Class Booked").waitFor();
+});
